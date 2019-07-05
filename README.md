8948be13cbd20421d5b79b0a9d33bff33dba169c
Gishu Pillai <gishu@hotmail.com>
2019-07-05 16:32:29 +0530

    create a service

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Angular Bootup repo
 
 ## create an angular component
 ng g[enerate] c[omponent] Sample
+
+## create a service
+ng g s[service] Sample
--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SampleService } from '../sample.service';
 
 @Component({
   selector: 'app-sample',
@@ -8,12 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class SampleComponent implements OnInit {
 
   title: String = "Sample title";
-  samples = ['Sample1', 'Sample2', 'Sample3']
-  constructor() { 
+  samples;
+  constructor(private _sampleService:SampleService) { 
     
   }
 
   ngOnInit() {
+    // load from a rest call for example
+    this.samples = this._sampleService.getSamplesFromAfar();
   }
 
 }
